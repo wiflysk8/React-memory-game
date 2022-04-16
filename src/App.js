2a5e61ef443131dd53e43c8d91cefbecbff12621
Luis Martínez <wifly.sk8@gmail.com>
2022-04-16 16:03:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Cards from "./components/Cards";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="c-title">REACT MEMORY GAME</h1>
+      <Cards />
+    </div>
+  );
+}
+
+export default App;
